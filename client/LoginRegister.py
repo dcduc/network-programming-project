ef5e4bac934208c861d2663c9d2ac944d71ad4e2
Giap <giapvu060604@gmail.com>
2024-06-12 17:50:18 +0700

    Update: Login/Register

--- client/LoginRegister.py
+++ client/LoginRegister.py
@@ -1,54 +1,63 @@
 from customtkinter import *
 
-def login():
-    print("Login")
+# def login():
+#     print("Login")
 
-def register():
-    print("Register")
+# def register():
+#     print("Register")
 
-def LoginRegisterUI(client_socket, root):
-    window = CTkToplevel(root)
-    window.geometry("700x300")
-    window.configure()
-    window.title("Login/Register")
+class LoginRegister(CTkFrame):
+    def __init__(self, parent):
+        self.parent = parent
+        CTkFrame.__init__(self, self.parent)
+        self.configure(height=300, width=700)
+        self.parent.geometry("700x300")
+        self.grid(row=0, column=0, sticky="nsew")
 
-    # Login UI
-    login_label = CTkLabel(window, text="Login", font=("Inria Sans Bold", 24), width=50, text_color="#FFFFFF", )
-    login_label.grid(row=0, column=0, columnspan=2, pady=10, padx=10)
+        # Login UI
+        self.login_label = CTkLabel(self, text="Login", font=("Inria Sans Bold", 24), width=50, text_color="#FFFFFF", )
+        self.login_label.grid(row=0, column=0, columnspan=2, pady=10, padx=10)
 
-    id_label = CTkLabel(window, text="ID:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
-    id_label.grid(row=1, column=0, sticky="w", padx=10, pady=10)
-    id_entry = CTkEntry(window, width=200, font=("Inria Sans", 12), placeholder_text="Enter ID...")
-    id_entry.grid(row=1, column=1, padx=10)
+        self.id_label = CTkLabel(self, text="ID:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
+        self.id_label.grid(row=1, column=0, sticky="w", padx=10, pady=10)
+        self.id_entry = CTkEntry(self, width=200, font=("Inria Sans", 12), placeholder_text="Enter ID...")
+        self.id_entry.grid(row=1, column=1, padx=10)
 
-    password_label = CTkLabel(window, text="Password:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
-    password_label.grid(row=2, column=0, sticky="w", padx=5, pady=5)
-    pass_entry = CTkEntry(window, show="*", width=200, font=("Inria Sans", 12), placeholder_text="Enter password...", )
-    pass_entry.grid(row=2, column=1, padx=10)
+        self.password_label = CTkLabel(self, text="Password:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
+        self.password_label.grid(row=2, column=0, sticky="w", padx=5, pady=5)
+        self.pass_entry = CTkEntry(self, show="*", width=200, font=("Inria Sans", 12), placeholder_text="Enter password...", )
+        self.pass_entry.grid(row=2, column=1, padx=10)
 
-    login_button = CTkButton(window, text="Login", width=30, font=("Inria Sans", 16), corner_radius=15, fg_color="#8AFF6C", text_color="#123456", )
-    login_button.grid(row=4, column=0, columnspan=2, pady=20)
+        self.login_button = CTkButton(self, text="Login", width=30, font=("Inria Sans", 16), corner_radius=15, fg_color="#8AFF6C", text_color="#123456", command = self.login())
+        self.login_button.grid(row=4, column=0, columnspan=2, pady=20)
 
-    # Register UI
-    register_label = CTkLabel(window, text="Register", font=("Inria Sans Bold", 24), width=50, text_color="#FFFFFF", )
-    register_label.grid(row=0, column=2, columnspan=2, pady=10, padx=10)
+        # Register UI
+        self.register_label = CTkLabel(self, text="Register", font=("Inria Sans Bold", 24), width=50, text_color="#FFFFFF", )
+        self.register_label.grid(row=0, column=2, columnspan=2, pady=10, padx=10)
 
-    id_label = CTkLabel(window, text="ID:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
-    id_label.grid(row=1, column=2, sticky="w", padx=10, pady=10)
-    id_entry = CTkEntry(window, width=200, font=("Inria Sans", 12), placeholder_text="Enter ID...")
-    id_entry.grid(row=1, column=3, padx=10)
+        self.id_label2 = CTkLabel(self, text="ID:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
+        self.id_label2.grid(row=1, column=2, sticky="w", padx=10, pady=10)
 
-    password_label = CTkLabel(window, text="Password:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
-    password_label.grid(row=2, column=2, sticky="w", padx=10, pady=10)
-    pass_entry = CTkEntry(window, show="*", width=200, font=("Inria Sans", 12), placeholder_text="Enter password...", )
-    pass_entry.grid(row=2, column=3, padx=10)
+        self.id_entry2 = CTkEntry(self, width=200, font=("Inria Sans", 12), placeholder_text="Enter ID...")
+        self.id_entry2.grid(row=1, column=3, padx=10)
 
-    confirm_password_label = CTkLabel(window, text="Confirm Password:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
-    confirm_password_label.grid(row=3, column=2, sticky="w", padx=10, pady=10)
-    confirm_pass_entry = CTkEntry(window, show="*", width=200, font=("Inria Sans", 12), placeholder_text="Enter password...", )
-    confirm_pass_entry.grid(row=3, column=3, padx=10)
+        self.password_label2 = CTkLabel(self, text="Password:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
+        self.password_label2.grid(row=2, column=2, sticky="w", padx=10, pady=10)
+        self.pass_entry2 = CTkEntry(self, show="*", width=200, font=("Inria Sans", 12), placeholder_text="Enter password...", )
+        self.pass_entry2.grid(row=2, column=3, padx=10)
 
-    register_button = CTkButton(window, text="Register", width=30, font=("Inria Sans", 16), corner_radius=15, fg_color="#8AFF6C", text_color="#123456", )
-    register_button.grid(row=4, column=2, columnspan=2, pady=20)
+        self.confirm_password_label = CTkLabel(self, text="Confirm Password:", anchor="e", font=("Inria Sans", 16), width=30, height=2, )
+        self.confirm_password_label.grid(row=3, column=2, sticky="w", padx=10, pady=10)
+        self.confirm_pass_entry = CTkEntry(self, show="*", width=200, font=("Inria Sans", 12), placeholder_text="Enter password...", )
+        self.confirm_pass_entry.grid(row=3, column=3, padx=10)
 
-    window.mainloop()
+        self.register_button = CTkButton(self, text="Register", width=30, font=("Inria Sans", 16), corner_radius=15, fg_color="#8AFF6C", text_color="#123456", command=self.register())
+        self.register_button.grid(row=4, column=2, columnspan=2, pady=20)
+
+    def login(self):
+        ID, passwd =  self.id_entry.get(), self.pass_entry.get()
+        # Truy vấn
+    
+    def register(self):
+        ID, passwd = self.id_entry2.get(), self.pass_entry2.get()
+        # Truy vấn
